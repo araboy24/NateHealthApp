250cbbdd04ed48f5deeb3ac2771edc888ff5b892
Eid Zaben <eid.s.zaben-1@ou.edu>
2020-10-01 20:44:48 +0300

    Goal frag updated

--- app/src/main/java/com/araboy/natehealthapp/GoalsFragment.java
+++ app/src/main/java/com/araboy/natehealthapp/GoalsFragment.java
@@ -4,15 +4,126 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Date;
+
 public class GoalsFragment extends Fragment {
+
+    FirebaseAuth fAuth;
+    FirebaseUser user;
+    FirebaseFirestore fStore;
+    String userId, dateS;
+
+
+    TextView txtGoal;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_goals, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_goals, container, false);
+
+        instantiate(view);
+
+        DocumentReference dStats = fStore.collection(userId).document("Survey");
+        if (dStats != null) {
+            try {
+                dStats.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        assert value != null;
+                        if(value != null) {
+                            if (value.get("Goal") != null) {
+
+                               txtGoal.setText("Weight Goal: " + value.get("Goal"));
+                            } else {
+                          //      txtCurrent.setText("DIDn't work");
+                            }
+
+                        }
+                    }
+                });
+            } catch(Exception e){}
+        }
+
+        return view;
+    }
+
+    public void instantiate(View view){
+        txtGoal = view.findViewById(R.id.txtWeightGoal);
+        fStore = FirebaseFirestore.getInstance();
+        fAuth= FirebaseAuth.getInstance();
+        user = fAuth.getCurrentUser();
+        if(user!= null) {
+            userId = user.getUid();
+        }
+        dateS = getDate(new Date());
+    }
+
+    public static String getDate(Date date) {
+        String month, day, year, dateWTime;
+        dateWTime = date.toString();
+        switch(dateWTime.substring(4, 7)) {
+            case "Jan":
+                month = "1";
+                break;
+            case "Feb":
+                month = "2";
+                break;
+            case "Mar":
+                month = "3";
+                break;
+            case "Apr":
+                month = "4";
+                break;
+            case "May":
+                month = "5";
+                break;
+            case "Jun":
+                month = "6";
+                break;
+            case "Jul":
+                month = "7";
+                break;
+            case "Aug":
+                month = "8";
+                break;
+            case "Sep":
+                month = "9";
+                break;
+            case "Oct":
+                month = "10";
+                break;
+            case "Nov":
+                month = "11";
+                break;
+            case "Dec":
+                month = "12";
+                break;
+            default:
+                month = "else";
+                break;
+        }
+
+        day = dateWTime.substring(8, 10);
+
+        year = dateWTime.substring(dateWTime.length()-4, dateWTime.length());
+
+        return month+"-"+day+"-"+year;
+
     }
 }
