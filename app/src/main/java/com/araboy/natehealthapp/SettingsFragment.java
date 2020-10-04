e38f543459693526e9f54d50443a0ce77f51794b
Eid Zaben <eid.s.zaben-1@ou.edu>
2020-10-04 21:09:08 +0300

    Settings connected to firebase

--- app/src/main/java/com/araboy/natehealthapp/SettingsFragment.java
+++ app/src/main/java/com/araboy/natehealthapp/SettingsFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 
 import androidx.annotation.NonNull;
@@ -15,19 +17,28 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingsFragment extends Fragment {
     SwitchMaterial switchNotif;
-    Button btnLogout;
+    Button btnLogout, btnSave;
+    RadioGroup rgUnits;
+    RadioButton rbMetric, rbImper;
 
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
     FirebaseUser user;
-    String userId, dateS;
-
+    String userId, dateS, units;
+    boolean isNotifOn;
 
     @Nullable
     @Override
@@ -35,21 +46,80 @@ public class SettingsFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_settings, container, false);
         instantiate(view);
 
+        //Initializing values on open
+        final DocumentReference docSettings = fStore.collection(userId).document("Settings");
+        if(docSettings!= null){
+            docSettings.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                    if(value != null){
+                        if (value.getString("Units") != null) {
+                            if(value.getString("Units").equals("Metric")){
+                                rbImper.setChecked(false);
+                                rbMetric.setChecked(true);
+                                units = "Metric";
+                            } else {
+                                rbImper.setChecked(true);
+                                rbMetric.setChecked(false);
+                                units = "Imperial";
+                            }
+                        }
+
+                        if(value.get("Notifications") != null){
+                            if((boolean)value.get("Notifications") == true){
+                                switchNotif.setChecked(true);
+                                isNotifOn = true;
+                            } else {
+                                switchNotif.setChecked(false);
+                                isNotifOn = false;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
         switchNotif.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(switchNotif.isChecked()){
                     switchNotif.setText("On");
+                    isNotifOn = true;
                 } else{
                     switchNotif.setText("Off");
+                    isNotifOn = false;
                 }
             }
         });
+        rgUnits.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(rgUnits.getCheckedRadioButtonId()){
+                    case R.id.rbMetric:
+                        units = "Metric";
+                        break;
+                    case R.id.rbImperial:
+                        units = "Imperial";
+                        break;
+                }
+            }
+        });
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ((HomeActivity)getActivity()).logout(view);
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> settings = new HashMap<>();
+                settings.put("Units", units);
+                settings.put("Notifications", isNotifOn);
+                docSettings.set(settings, SetOptions.merge());
             }
         });
 
@@ -59,6 +129,10 @@ public class SettingsFragment extends Fragment {
     public void instantiate(View view) {
         switchNotif = view.findViewById(R.id.switchNotif);
         btnLogout = view.findViewById(R.id.btnLogOutSettings);
+        btnSave = view.findViewById(R.id.btnSave);
+        rgUnits = view.findViewById(R.id.rgUnits);
+        rbImper = view.findViewById(R.id.rbImperial);
+        rbMetric = view.findViewById(R.id.rbMetric);
 
         fStore = FirebaseFirestore.getInstance();
         fAuth= FirebaseAuth.getInstance();
