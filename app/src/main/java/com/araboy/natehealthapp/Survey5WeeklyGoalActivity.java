8f933f4fd47867ec71ddcd6e0fa95a50bcbe2474
Eid Zaben <eid.s.zaben-1@ou.edu>
2020-10-06 22:42:53 +0300

    Calorie calculator added and stored in database

--- app/src/main/java/com/araboy/natehealthapp/Survey5WeeklyGoalActivity.java
+++ app/src/main/java/com/araboy/natehealthapp/Survey5WeeklyGoalActivity.java
@@ -20,6 +20,13 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.SetOptions;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -30,6 +37,10 @@ public class Survey5WeeklyGoalActivity extends AppCompatActivity {
     Button btnFinish;
     String iden = "";
 
+    //Daily intake
+    double caloriesADay, caloriesMaintain, caloriesChange;
+
+    String weeklyGoalS = "";
 
 
 
@@ -39,6 +50,7 @@ public class Survey5WeeklyGoalActivity extends AppCompatActivity {
     FirebaseUser user;
     String userId;
     DocumentReference documentReference, goalsDocRef;
+    DocumentReference dailyReqDocRef, getBodyStatsDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,6 +147,8 @@ public class Survey5WeeklyGoalActivity extends AppCompatActivity {
                                 goal = "Gain 0.5 lbs a week";
                             }
                         }
+
+                        weeklyGoalS = goal;
                         weeklyGoalGoals.put("Title", goal);
                         weeklyGoalGoals.put("Desc", goal);
                         DocumentReference docGoal = goalsDocRef.collection("Weekly Goals").document(goal);
@@ -145,10 +159,158 @@ public class Survey5WeeklyGoalActivity extends AppCompatActivity {
                     } else {
                         Toast.makeText(Survey5WeeklyGoalActivity.this, "Select a Goal", Toast.LENGTH_SHORT).show();
                     }
+
+                    saveDailyMacros();
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 }
             });
         }
 
     }
+
+    public void saveDailyMacros(){
+        dailyReqDocRef = fStore.collection(userId).document("Daily Requirements");
+        getBodyStatsDoc = fStore.collection(userId).document("Survey");
+        //Getting the body info
+        getBodyStatsDoc.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                double weight, height, age, activityLevel, weeklyGoal;
+                double caloriesBefore;
+            //    Date dob = (Date)value.get("DOB");
+                String dobS = value.getString("SDOB");
+                weight = (Double) value.get("Weight (kg)");
+                height = (Double) value.get("Height (cm)");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                Date birthDate = null;
+                try {
+                    birthDate = sdf.parse(dobS);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Age ageA = calculateAge(birthDate);
+                age = ageA.getYears();
+                switch(value.getString("Activity Level")){
+                    case "Not Active":
+                        activityLevel = 1;
+                        break;
+                    case "Lightly Active":
+                        activityLevel = 1.11;
+                        break;
+                    case "Active":
+                        activityLevel = 1.25;
+                        break;
+                    case "Very Active":
+                        activityLevel = 1.48;
+                        break;
+                    default:
+                        activityLevel = 1;
+                }
+         //EER = 661.8 - 9.53*age (years) + PAL *(15.91* weight (kg) + 539.6* height (m)),
+         // where PAL = 1 if sedentary, 1.11 if low active, 1.25 if active, and 1.48 if very active.
+                caloriesBefore = 661.8 - 9.53*age + activityLevel * (15.91 * weight + 539.6 * (height/100));
+                double calDif = 0;
+                switch(weeklyGoalS){
+                    case "Maintain Weight":
+                        calDif = 0;
+                        break;
+                    case "Gain 0.5 lbs a week":
+                        calDif = 250;
+                        break;
+                    case "Lose 0.5 lbs a week":
+                        calDif = -250;
+                        break;
+                    case "Gain 1 lb a week":
+                        calDif = 500;
+                        break;
+                    case "Lose 1 lb a week":
+                        calDif = -500;
+                        break;
+                    default:
+                        calDif = 0;
+                        break;
+                }
+
+                caloriesADay = caloriesBefore + calDif;
+                caloriesMaintain = caloriesBefore;
+                caloriesChange = calDif;
+
+                Map<String, Object> dailyMacs = new HashMap<>();
+                dailyMacs.put("Calories Maintain", (int)caloriesMaintain);
+                dailyMacs.put("Calories Difference", caloriesChange);
+                dailyMacs.put("Calories Adjusted", (int)caloriesADay);
+                dailyReqDocRef.set(dailyMacs, SetOptions.merge());
+
+
+                //TODO Calculate other macros later
+
+            }
+        });
+
+
+
+
+    }
+
+    private static Age calculateAge(Date birthDate)
+    {
+        int years = 0;
+        int months = 0;
+        int days = 0;
+
+        //create calendar object for birth day
+        Calendar birthDay = Calendar.getInstance();
+        birthDay.setTimeInMillis(birthDate.getTime());
+
+        //create calendar object for current day
+        long currentTime = System.currentTimeMillis();
+        Calendar now = Calendar.getInstance();
+        now.setTimeInMillis(currentTime);
+
+        //Get difference between years
+        years = now.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR);
+        int currMonth = now.get(Calendar.MONTH) + 1;
+        int birthMonth = birthDay.get(Calendar.MONTH) + 1;
+
+        //Get difference between months
+        months = currMonth - birthMonth;
+
+        //if month difference is in negative then reduce years by one
+        //and calculate the number of months.
+        if (months < 0)
+        {
+            years--;
+            months = 12 - birthMonth + currMonth;
+            if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
+                months--;
+        } else if (months == 0 && now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
+        {
+            years--;
+            months = 11;
+        }
+
+        //Calculate the days
+        if (now.get(Calendar.DATE) > birthDay.get(Calendar.DATE))
+            days = now.get(Calendar.DATE) - birthDay.get(Calendar.DATE);
+        else if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
+        {
+            int today = now.get(Calendar.DAY_OF_MONTH);
+            now.add(Calendar.MONTH, -1);
+            days = now.getActualMaximum(Calendar.DAY_OF_MONTH) - birthDay.get(Calendar.DAY_OF_MONTH) + today;
+        }
+        else
+        {
+            days = 0;
+            if (months == 12)
+            {
+                years++;
+                months = 0;
+            }
+        }
+        //Create new Age object
+        return new Age(days, months, years);
+    }
+
+
+
 }
